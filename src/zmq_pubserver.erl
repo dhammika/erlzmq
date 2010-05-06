1202535353c4f90fafa4cb7b9d067f39de1df80c
dhammika <dhammika@gmail.com>
2010-05-05 21:27:39 -0700

    add pub/sub example, update README

---
+++ src/zmq_pubserver.erl
@@ -0,0 +1,32 @@
+% Basic pub server for testing.
+
+-module(zmq_pubserver).
+-export([run/0]).
+
+run() ->
+    zmq:start_link(),
+    zmq:init(1,1,0),
+    case zmq:socket(zmq_pub) of
+        {ok, Socket} -> 
+            zmq:bind(Socket, term_to_binary("tcp://127.0.0.1:5550")),
+            send(Socket);
+        other -> other
+    end.
+
+send(Socket) ->
+    send(Socket, 1).
+
+send(Socket, MsgIndex) ->
+    Data = lists:flatten(io_lib:format("Msg ~B", [MsgIndex])),
+    io:format("Snd ~p ~n", [Data]),
+    case zmq:send(Socket, list_to_binary(Data)) of 
+        ok -> 
+            sleep(1000),
+            send(Socket, MsgIndex + 1);
+        other -> other
+    end.
+
+sleep(MilliSecs) ->
+    receive 
+    after MilliSecs -> true
+    end.
---
+++ src/zmq_subclient.erl
@@ -0,0 +1,23 @@
+% Basic sub client for testing.
+
+-module(zmq_subclient).
+-export([run/0]).
+
+run() ->
+    zmq:start_link(),
+    zmq:init(1,1,0),
+    case zmq:socket(zmq_sub) of
+        {ok, Socket} -> 
+            zmq:sockopt(set, {Socket, zmq_subscribe, <<>>}),
+            zmq:connect(Socket, term_to_binary("tcp://127.0.0.1:5550")),
+            read(Socket);
+        other -> other
+    end.
+
+read(Socket) ->
+    case zmq:recv(Socket) of 
+        {ok, Data} -> 
+            io:format("Rcv ~p ~n", [binary_to_list(Data)]),
+            read(Socket);
+        other -> other
+    end.
